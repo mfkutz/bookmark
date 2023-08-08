b057f715246ef17c84d9b114252ae2917928b82c
Martin Kutzner <mfkutz@gmail.com>
2023-08-08 20:22:39 -0300

    logic

--- src/components/FeaturesOptions/FeaturesOptions.jsx
+++ src/components/FeaturesOptions/FeaturesOptions.jsx
@@ -3,19 +3,37 @@ import './FeaturesOptions.css'
 
 const FeaturesOptions = () => {
 
-  const [btnActive, setBtnActive] = useState(false)
+  const [btnOne, setBtnOne] = useState(true)
+  const [btnTwo, setBtnTwo] = useState(false)
+  const [btnThree, setBtnThree] = useState(false)
+
+  function selectorOne() {
+    setBtnOne(true)
+    setBtnTwo(false)
+    setBtnThree(false)
+  }
+  function selectorTwo() {
+    setBtnOne(false)
+    setBtnTwo(true)
+    setBtnThree(false)
+  }
+  function selectorThree() {
+    setBtnOne(false)
+    setBtnTwo(false)
+    setBtnThree(true)
+  }
 
   return (
     <section id='featuresOp'>
 
       <div className="menu_features">
-        <div className={`btn_features ${btnActive ? '' : 'active'}`} onClick={() => setBtnActive(true)}>
+        <div className={`btn_features ${btnOne ? 'active' : ''}`} onClick={selectorOne}>
           Simple Bookmarking
         </div>
-        <div className='btn_features'>
+        <div className={`btn_features ${btnTwo ? 'active' : ''}`} onClick={selectorTwo}>
           Speedy Searching
         </div>
-        <div className='btn_features'>
+        <div className={`btn_features ${btnThree ? 'active' : ''}`} onClick={selectorThree}>
           Easy Sharing
         </div>
       </div>
