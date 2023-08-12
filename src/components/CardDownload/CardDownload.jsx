f1999302addfb235e3ad3c95b851ccd680d5f906
Martin Kutzner <mfkutz@gmail.com>
2023-08-12 10:21:04 -0300

    alt

--- src/components/CardDownload/CardDownload.jsx
+++ src/components/CardDownload/CardDownload.jsx
@@ -4,14 +4,14 @@ import { background } from '../../assets/index'
 const CardDownload = ({ logo, title, version }) => {
     return (
         <>
-            <img src={logo} alt="" className='logo_nav' />
+            <img src={logo} alt="logo" className='logo_nav' />
             <h3 className='download_title'>
                 {title}
             </h3>
             <p className='p_version'>
                 Minimum version {version}
             </p>
-            <img src={background} alt="" className='separator' />
+            <img src={background} alt="background" className='separator' />
             <div>
                 <a href="#" className='btn_install'>
                     Add & Install Extension
